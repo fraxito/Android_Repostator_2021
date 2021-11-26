6f4467bf162aa061d09caeca644ed5f72046ba88
xp <xp@CETYSDAM00>
2021-11-26 11:03:57 +0100

    terminado el mainactivity

--- app/src/main/java/com/example/repostator/MainActivity.java
+++ app/src/main/java/com/example/repostator/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.repostator;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,6 +21,10 @@ import com.example.repostator.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,14 +32,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        final Intent ventanaCaptura = new Intent(this, Repostaje.class);
+
         setContentView(R.layout.activity_main);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(ventanaCaptura);
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        ArrayList<String> listadoRepostajes = new ArrayList<>();
+        ListView listaVista;
+        SharedPreferences sp = getSharedPreferences("datos", Context.MODE_PRIVATE);
+
+        int size = sp.getInt("listado_size", 0);
+        String precio = "";
+        String kilometros = "";
+        String litros = "";
+        String dia = "";
+        String mes = "";
+        String year = "";
+        double costeTotal = 0;
+
+        for (int i=1; i<= size; i++){
+            precio = sp.getString("precio_"+i, "0");
+            kilometros = sp.getString("kilometros_"+i, "0");
+            litros = sp.getString("litros_"+i, "0");
+            costeTotal = Double.valueOf(precio) * Double.valueOf(litros);
+            dia = sp.getString("dia_"+i, "0");
+            mes = sp.getString("mes_"+i, "0");
+            year = sp.getString("year_"+i, "0");
+
+            listadoRepostajes.add(kilometros + " Km " + litros
+                    + " L " + precio + " €/L coste: " +
+                    String.format("%.2f",costeTotal) +
+                    " fecha: " + dia + "/" + mes + "/" + year);
+        }
+
+        listaVista = findViewById(R.id.marcoLista); //enlaza el controlador con la vista
+
+        //crea un objeto para comunicar el array list con el listview
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                listadoRepostajes
+        );
+
+        //enlaza el arraylist con el listview para que se vea en pantalla
+        listaVista.setAdapter(arrayAdapter);
+
+
     }
 
 }
